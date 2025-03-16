c62aa9d5b6099cfa5fe788de3e73cd8e70877dd4
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2025-03-16 16:58:13 +0200

    add grid for character inventory (equip and list items)

--- app/(tabs)/character_profile.tsx
+++ app/(tabs)/character_profile.tsx
@@ -42,7 +42,8 @@ export default function CharacterProfile() {
                 <Image
                     style={{
                         width: '60%',
-                        height: '60%'
+                        height: '60%',
+                        opacity: currentList === CURRENT_PAGE.CHARACTER_INVENTORY ? 0.3 : 1
                     }}
                     source={characterStats.model}
                 />
@@ -53,51 +54,32 @@ export default function CharacterProfile() {
                     <View style={{
                         flexDirection: 'row',
                         gap: 10,
-                        // justifyContent: "space-between"
                         justifyContent: 'space-around',
                         backgroundColor: 'grey',
                         paddingBottom: 10,
                         paddingTop: 10,
-                        // height: 200,
-                        // width: '100%'
+
                     }}>
-                        <ImageBackground
-                        // style={{
-                        //     width: '100%',
-                        //     height: '80%'
-                        // }}
-                        // source={buttonMask}
-                        // resizeMode="cover"
-                        >
-                            {/* <Image
-                            style={{
-                                width: '80%',
-                                height: '100%',
+
+                        <TouchableOpacity
+                            onPress={() => {
+                                setCurrentList(CURRENT_PAGE.CHARACTER_STATS)
                             }}
-                            source={buttonMask} /> */}
-                            <TouchableOpacity
-                                onPress={() => {
-                                    setCurrentList(CURRENT_PAGE.CHARACTER_STATS)
-                                }}
-                                style={{
-                                    // backgroundColor: currentList === CURRENT_PAGE.CHARACTER_STATS ? 'green' : 'white',
-                                    padding: 6
-                                    // width: '50%'
-                                }}>
+                            style={{
+                                padding: 6
+                            }}>
 
-                                <Text style={{
-                                    color: currentList === CURRENT_PAGE.CHARACTER_STATS ? 'white' : 'black'
-                                }}>{CURRENT_PAGE.CHARACTER_STATS}</Text>
+                            <Text style={{
+                                color: currentList === CURRENT_PAGE.CHARACTER_STATS ? 'white' : 'black'
+                            }}>{CURRENT_PAGE.CHARACTER_STATS}</Text>
 
 
-                            </TouchableOpacity>
-                        </ImageBackground>
+                        </TouchableOpacity>
                         <TouchableOpacity
                             onPress={() => {
                                 setCurrentList(CURRENT_PAGE.CHARACTER_INVENTORY)
                             }}
                             style={{
-                                // backgroundColor: currentList === CURRENT_PAGE.CHARACTER_INVENTORY ? 'green' : 'white',
                                 padding: 6
                             }}
                         >
@@ -109,10 +91,13 @@ export default function CharacterProfile() {
                         </TouchableOpacity>
                     </View>
                     <View style={{
-                        flexDirection: 'row'
+                        flexDirection: 'row',
+                        width: '100%',
+                        height: '50%'
                     }}>
                         <View style={{
                             margin: 10,
+                            width: currentList === CURRENT_PAGE.CHARACTER_STATS ? '100%' : '0%',
                             opacity: currentList === CURRENT_PAGE.CHARACTER_STATS ? 1 : 0, // Элемент остается на месте, но невидим
                             pointerEvents: currentList === CURRENT_PAGE.CHARACTER_STATS ? "auto" : "none", // Блокирует клики, если скрыт
 
@@ -127,27 +112,97 @@ export default function CharacterProfile() {
                         </View>
                         <View style={{
                             margin: 10,
+
+                            width: currentList === CURRENT_PAGE.CHARACTER_INVENTORY ? '100%' : '0%',
                             opacity: currentList === CURRENT_PAGE.CHARACTER_INVENTORY ? 1 : 0, // Элемент остается на месте, но невидим
                             pointerEvents: currentList === CURRENT_PAGE.CHARACTER_INVENTORY ? "auto" : "none", // Блокирует клики, если скрыт
+                            flexDirection: 'row',
+                            justifyContent: 'space-between'
                         }}>
-                            <View>
-                                <Text>Weapon:</Text>
-                                {characterInventory.filter((weaponType) => weaponType.type === INVENTORY_ITEM_TYPE.WEAPON).map((element) =>
-                                    <Text key={element.id}>{element.name} x {element.value}</Text>
-                                )}
+                            <View style={{
+                                width: '50%'
+                            }}>
+                                <Text style={{
+                                    fontSize: 16,
+                                    fontWeight: 'bold'
+                                }}>Equip</Text>
                             </View>
-                            <View>
-                                <Text>Armor:</Text>
-                                {characterInventory.filter((armorType) => armorType.type === INVENTORY_ITEM_TYPE.ARMOR).map((element) =>
-                                    <Text key={element.id}>{element.name} x {element.value}</Text>
-                                )}
+                            <View style={{
+                                width: '50%'
+                            }}>
+                                <View style={{
+                                }}>
+                                    <Text style={{
+                                        fontSize: 16,
+                                        fontWeight: 'bold'
+                                    }}>Weapon:</Text>
+                                    <View>
+                                        {[
+                                            ...characterInventory
+                                                .filter((item) => item.type === INVENTORY_ITEM_TYPE.WEAPON)
+                                                .map((element) => ({
+                                                    text: `${element.name} x ${element.value}`,
+                                                    opacity: 1
+                                                })),
+                                            ...Array(3).fill({ text: "--Empty--", opacity: 0.5 }) // Добавляем "Empty" с полупрозрачностью
+                                        ]
+                                            .slice(0, 3) // Обрезаем до 3 элементов
+                                            .map((item, index) => (
+                                                <Text key={index} style={{ opacity: item.opacity }}>
+                                                    {item.text}
+                                                </Text>
+                                            ))}
+                                    </View>
+                                </View>
+                                <View>
+                                    <Text style={{
+                                        fontSize: 16,
+                                        fontWeight: 'bold'
+                                    }}>Armor:</Text>
+                                    <View>
+                                        {[
+                                            ...characterInventory
+                                                .filter((item) => item.type === INVENTORY_ITEM_TYPE.ARMOR)
+                                                .map((element) => ({
+                                                    text: `${element.name} x ${element.value}`,
+                                                    opacity: 1
+                                                })),
+                                            ...Array(3).fill({ text: "--Empty--", opacity: 0.5 }) // Добавляем "Empty" с полупрозрачностью
+                                        ]
+                                            .slice(0, 3) // Обрезаем до 3 элементов
+                                            .map((item, index) => (
+                                                <Text key={index} style={{ opacity: item.opacity }}>
+                                                    {item.text}
+                                                </Text>
+                                            ))}
+                                    </View>
+
+                                </View>
+                                <View>
+                                    <Text style={{
+                                        fontSize: 16,
+                                        fontWeight: 'bold'
+                                    }}>Consumbles:</Text>
+                                    <View>
+                                        {[
+                                            ...characterInventory
+                                                .filter((item) => item.type === INVENTORY_ITEM_TYPE.CONSUMBLES)
+                                                .map((element) => ({
+                                                    text: `${element.name} x ${element.value}`,
+                                                    opacity: 1
+                                                })),
+                                            ...Array(3).fill({ text: "--Empty--", opacity: 0.5 }) // Добавляем "Empty" с полупрозрачностью
+                                        ]
+                                            .slice(0, 3) // Обрезаем до 3 элементов
+                                            .map((item, index) => (
+                                                <Text key={index} style={{ opacity: item.opacity }}>
+                                                    {item.text}
+                                                </Text>
+                                            ))}
+                                    </View>
+                                </View>
                             </View>
-                            <View>
-                                <Text>Consumbles:</Text>
-                                {characterInventory.filter((consumblesType) => consumblesType.type === INVENTORY_ITEM_TYPE.CONSUMBLES).map((element) =>
-                                    <Text key={element.id}>{element.name} x {element.value}</Text>
-                                )}
-                            </View>
+
                         </View>
                     </View>
                 </View>
