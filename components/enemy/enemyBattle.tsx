476ee424be4f3bf5cb3fcd09fe8a017f8d5d1fed
Dmitrii <dmitrii.konevnin@gmail.com>
2026-09-05 22:42:31 +0300

    created enemy battle component

---
+++ components/enemy/enemyBattle.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+export default function EnemyBattle() {
+  return (
+    <View>
+        <Text>Enemy battle</Text>
+    </View>
+  )
+}
