e45728247c12c08954c5be6d5038165c2be96631
Dmitrii <dmitrii.konevnin@gmail.com>
2026-09-06 19:56:55 +0300

    created shared folder for reused components

---
+++ components/shared/healths.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Healths() {
+  return    (
+    <View>
+      <Text>healths</Text>
+    </View>
+  )
+}
