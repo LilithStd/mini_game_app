63d7a6999696d5fd3c6ab2d3ab9359ed82b0d292
Dmitrii <dmitrii.konevnin@gmail.com>
2026-09-06 19:53:36 +0300

    updated battles components

--- components/enemy/enemy_battle.tsx
+++ components/enemy/enemy_battle.tsx
@@ -1,6 +1,8 @@
+import { useBattleStore } from '@/store/battle/battle_store';
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 export default function EnemyBattle() {
+  const enemyStats = useBattleStore((state) => state.enemy);
   return (
     <View>
         <Text>Enemy battle</Text>
--- components/player/character_battle.tsx
+++ components/player/character_battle.tsx
@@ -1,7 +1,9 @@
+import { useBattleStore } from '@/store/battle/battle_store';
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
 export default function CharacterBattle() {
+  const characterStats = useBattleStore((state) => state.character);
   return (
     <View>
         <Text>Character battle</Text>
