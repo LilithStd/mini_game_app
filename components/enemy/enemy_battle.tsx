e49c8698d547cca6ebab8c312866b95d1ea3d98d
Dmitrii <dmitrii.konevnin@gmail.com>
2026-09-07 21:18:55 +0300

    updated enemy battle component

--- components/enemy/enemy_battle.tsx
+++ components/enemy/enemy_battle.tsx
@@ -1,11 +1,13 @@
 import { useBattleStore } from '@/store/battle/battle_store';
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import Healths from '../shared/healths';
 export default function EnemyBattle() {
   const enemyStats = useBattleStore((state) => state.enemy);
   return (
     <View>
         <Text>Enemy battle</Text>
+        <Healths values={{ current: enemyStats.stats.healPoints.current, max: enemyStats.stats.healPoints.max }} />
     </View>
   )
 }
