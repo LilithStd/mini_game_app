4158dce4980171b35d1bbd9fea813e89ad31879c
Dmitrii <dmitrii.konevnin@gmail.com>
2026-09-07 21:14:40 +0300

    updated character battle component

--- components/player/character_battle.tsx
+++ components/player/character_battle.tsx
@@ -1,12 +1,14 @@
 import { useBattleStore } from '@/store/battle/battle_store';
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import Healths from '../shared/healths';
 
 export default function CharacterBattle() {
   const characterStats = useBattleStore((state) => state.character);
   return (
     <View>
         <Text>Character battle</Text>
+        <Healths values={{ current: characterStats.healPoints.current, max: characterStats.healPoints.max }} />
     </View>
   )
 }
