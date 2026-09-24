de339a8f82c2ddf1bdd6f19584186b56eba0a74b
Dmitrii <dmitrii.konevnin@gmail.com>
2026-09-24 20:22:22 +0300

    updated battle screen components

--- components/enemy/enemy_battle.tsx
+++ components/enemy/enemy_battle.tsx
@@ -1,14 +1,18 @@
 import { useBattleStore } from '@/store/battle/battle_store';
 import React from 'react'
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet, Image, useWindowDimensions } from 'react-native'
 import Healths from '../shared/healths';
+
 export default function EnemyBattle() {
   const enemyStats = useBattleStore((state) => state.enemy);
+  const { height } = useWindowDimensions();
+  const previewHeight = height * 0.66;
+
   return (
     <View style={styles.mainContainer}>
         <Text>Enemy battle</Text>
         <Healths values={{ current: enemyStats.stats.healPoints.current, max: enemyStats.stats.healPoints.max }} />
-        <View style={styles.imageContainer}>
+        <View style={[styles.imageContainer, { height: previewHeight }]}>
           <Image source={enemyStats.model} style={styles.image} resizeMode="contain" />
         </View>
         
