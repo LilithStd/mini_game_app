1e020954815cf4ade4a605feaafb9d944cd1bed6
Dmitrii <dmitrii.konevnin@gmail.com>
2026-09-25 21:46:13 +0300

    updated styles in enemy battle component

--- components/enemy/enemy_battle.tsx
+++ components/enemy/enemy_battle.tsx
@@ -6,14 +6,17 @@ import Healths from '../shared/healths';
 export default function EnemyBattle() {
   const enemyStats = useBattleStore((state) => state.enemy);
   const { height } = useWindowDimensions();
-  const previewHeight = height * 0.66;
+  const previewHeight = height * 0.54;
 
   return (
     <View style={styles.mainContainer}>
         <Text>Enemy battle</Text>
-        <Healths values={{ current: enemyStats.stats.healPoints.current, max: enemyStats.stats.healPoints.max }} />
+        <View style={styles.healthContainer}>
+          <Healths values={{ current: enemyStats.stats.healPoints.current, max: enemyStats.stats.healPoints.max }} />
+        </View>
+        
         <View style={[styles.imageContainer, { height: previewHeight }]}>
-          <Image source={enemyStats.model} style={styles.image} resizeMode="contain" />
+          <Image source={enemyStats.model} style={styles.image} resizeMode="cover" />
         </View>
         
     </View>
@@ -24,15 +27,18 @@ const styles = StyleSheet.create({
   mainContainer: {
     width: '100%',
     padding: 10,
-    paddingBottom: 30,
+    // paddingBottom: 30,
     backgroundColor: 'white',
   },
   imageContainer: {
     width: '100%',
-    aspectRatio: 1024/1536,
+    // aspectRatio: 1024/1536,
     backgroundColor: 'grey',
     overflow: 'hidden',
     borderRadius: 20,
+  },
+  healthContainer: {
+    marginBottom: 10,
   },
   image: {
     width: '100%',
