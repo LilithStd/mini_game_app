2b61c0de3d3f7b84e9b3a422c21932167c2e0c7d
Dmitrii <dmitrii.konevnin@gmail.com>
2026-09-26 20:39:27 +0300

    updated random action component

--- components/battle_screen/random_action.tsx
+++ components/battle_screen/random_action.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { MultilanguageType } from '@/constants/global_types';
+import { useGlobalStore } from '@/store/global_store';
 
 interface RandomActionProps {
     randomActionsArray: MultilanguageType[]
 }
 
 export default function RandomAction({ randomActionsArray }: RandomActionProps) {
-  return (
-    <View style={styles.container}>
-      <Text>Random Action</Text>
-    </View>
-  );
+    const currentLanguage = useGlobalStore(state => state.currentLanguage);
+
+    const methodToRandomAction = () => {
+        const randomIndex = Math.floor(Math.random() * randomActionsArray.length);
+        return randomActionsArray[randomIndex][currentLanguage].title;
+    };
+
+    return (
+        <View style={styles.container}>
+        <Text>Random Action:</Text>
+        <Text>{methodToRandomAction()}</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
